1c59f0a8f6c4cbedafc2e0ede92a414e35f59322
gabrielelerch@yahoo.com <98014975+Zeoane@users.noreply.github.com>
2026-07-28 20:41:20 +0200

    firebase config

--- simple-crm/src/app/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyC99RKg3n-P9Ly82Jc0te_rsvdKbQlguJw',
-  authDomain: 'simple-crm-8a89b.firebaseapp.com',
-  projectId: 'simple-crm-8a89b',
-  storageBucket: 'simple-crm-8a89b.firebasestorage.app',
-  messagingSenderId: '780149218435',
-  appId: '1:780149218435:web:853495c808781006868fb6',
-};
